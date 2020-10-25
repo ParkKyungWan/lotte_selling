cdaba378c0a4a9f8322eb9e5973f8c626ad5c42d
ParkKyungWan <pkw8117197@gmail.com>
2020-10-25 11:19:10 +0900

    asdffsdsdfsfd

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
 function App() {
   return (
     <React.Fragment>
-      <Switch basename = {process.env.PUBLIC_URL}>
-        <Router>
-          <Switch>
+      <Router>
+            <Switch basename = {process.env.PUBLIC_URL}>
               <Route exact path="/" component={Song}/>  
               <Route exact path="/detail"
                     render={ () => <Young id=""/> }       //props 필요하면 이렇게 보내면 댑니당.
               />
-          </Switch>
-        </Router>
-      </Switch>
+            </Switch>
+      </Router>
     </React.Fragment>
   );
 }
